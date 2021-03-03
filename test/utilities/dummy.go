c89d7d526a90162dbe43728a6c72c7e7f279bbb2
AsterNighT <klxjt99@outlook.com>
2021-03-03 10:51:45 +0800

    temp
    Signed-off-by: AsterNighT <klxjt99@outlook.com>

---
+++ test/utilities/dummy.go
@@ -0,0 +1,14 @@
+package dummy
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM, syscall.SIGSTOP)
+	for {
+	}
+}
