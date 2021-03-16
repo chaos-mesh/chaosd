4160e3795263cf268f31247b849e88b71144fb33
Andrewmatilde <davis6813585853062@outlook.com>
2021-03-16 15:21:53 +0800

    add boilerplate
    Signed-off-by: Andrewmatilde <davis6813585853062@outlook.com>

--- pkg/server/chaosd/disk.go
+++ pkg/server/chaosd/disk.go
@@ -1,3 +1,16 @@
+// Copyright 2020 Chaos Mesh Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package chaosd
 
 import (
@@ -7,12 +20,11 @@ import (
 	"os/exec"
 	"strconv"
 
-	"k8s.io/apimachinery/pkg/util/rand"
-
 	"github.com/google/uuid"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/log"
 	"go.uber.org/zap"
+	"k8s.io/apimachinery/pkg/util/rand"
 
 	"github.com/chaos-mesh/chaosd/pkg/core"
 )
@@ -117,10 +129,12 @@ func (s *Server) DiskFill(fill *core.DiskCommand) (uid string, err error) {
 				}
 				fill.Path = wd + tempFilename
 				f, err := os.Create(fill.Path)
-				f.Close()
 				if err != nil {
 					log.Error("unexpected err when creating temp file", zap.Error(err))
 					return uid, err
+				}
+				if f != nil {
+					_ = f.Close()
 				}
 				break
 			}
