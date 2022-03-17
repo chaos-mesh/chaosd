e0da300a43415a294675d2786691da83d07d2d68
xixi <i@hexilee.me>
2022-03-17 14:12:20 +0800

    bump cobra to 1.4.0
    Signed-off-by: xixi <i@hexilee.me>

--- cmd/completion/completion.go
+++ cmd/completion/completion.go
@@ -27,30 +27,44 @@ func NewCompletionCommand() *cobra.Command {
 		Use:   "completion [bash|zsh|fish|powershell]",
 		Short: "Generate completion script",
 		Long: `To load completions:
-	Bash:
-	$ source <(chaosd completion bash)
-	# To load completions for each session, execute once:
-	Linux:
-	  $ ./bin/chaosd completion bash > /etc/bash_completion.d/chaosd
-	MacOS:
-	  $ ./bin/chaosd completion bash > /usr/local/etc/bash_completion.d/chaosd
-	Zsh:
-	$ compdef _chaosd ./bin/chaosd
-	# If shell completion is not already enabled in your environment you will need
-	# to enable it.  You can execute the following once:
-	$ echo "autoload -U compinit; compinit" >> ~/.zshrc
-	# To load completions for each session, execute once:
-	$ ./bin/chaosd completion zsh > "${fpath[1]}/_chaosd"
-	# You will need to start a new shell for this setup to take effect.
-	Fish:
-	$ ./bin/chaosd completion fish | source
-	# To load completions for each session, execute once:
-	$ ./bin/chaosd completion fish > ~/.config/fish/completions/chaosd.fish
-	Powershell:
-	PS> ./bin/chaosd completion powershell | Out-String | Invoke-Expression
-	# To load completions for every new session, run:
-	PS> ./bin/chaosd completion powershell > chaosd.ps1
-	# and source this file from your powershell profile.
+Bash:
+
+$ source <(./bin/chaosd completion bash)
+
+# To load completions for each session, execute once:
+Linux:
+	$ ./bin/chaosd completion bash > /etc/bash_completion.d/chaosd
+MacOS:
+	$ ./bin/chaosd completion bash > /usr/local/etc/bash_completion.d/chaosd
+
+Zsh:
+
+$ compdef _chaosd ./bin/chaosd
+
+# If shell completion is not already enabled in your environment you will need
+# to enable it.  You can execute the following once:
+
+$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+
+# To load completions for each session, execute once:
+$ ./bin/chaosd completion zsh > "${fpath[1]}/_chaosd"
+
+# You will need to start a new shell for this setup to take effect.
+
+Fish:
+
+$ ./bin/chaosd completion fish | source
+
+# To load completions for each session, execute once:
+$ ./bin/chaosd completion fish > ~/.config/fish/completions/chaosd.fish
+
+Powershell:
+
+PS> ./bin/chaosd completion powershell | Out-String | Invoke-Expression
+
+# To load completions for every new session, run:
+PS> ./bin/chaosd completion powershell > chaosd.ps1
+# and source this file from your powershell profile.
 	`,
 		DisableFlagsInUseLine: true,
 		ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
