4594948db24878289371fc01c5ec33d204a91bcf
FingerLeader <wanxfinger@gmail.com>
2022-04-18 22:42:31 +0800

    make boilerplate
    Signed-off-by: FingerLeader <wanxfinger@gmail.com>

--- pkg/server/chaosd/host_unix.go
+++ pkg/server/chaosd/host_unix.go
@@ -1,4 +1,3 @@
-//go:build aix || darwin || dragonfly || freebsd || (js && wasm) || linux || nacl || netbsd || openbsd || solaris
 // +build aix darwin dragonfly freebsd js,wasm linux nacl netbsd openbsd solaris
 
 // Copyright 2021 Chaos Mesh Authors.
