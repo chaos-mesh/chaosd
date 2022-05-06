c5b408354abaf59d9fd6d54a4d461a2b4606c6e9
xixi <i@hexilee.me>
2022-05-06 20:49:08 +0800

    change default threads and tps
    Signed-off-by: xixi <i@hexilee.me>

--- cmd/attack/kafka.go
+++ cmd/attack/kafka.go
@@ -86,8 +86,8 @@ func NewKafkaFloodCommand(dep fx.Option, options *core.KafkaCommand) *cobra.Comm
 	cmd.Flags().StringVarP(&options.Username, "username", "u", "", "the username of kafka client")
 	cmd.Flags().StringVarP(&options.Password, "password", "p", "", "the password of kafka client")
 	cmd.Flags().UintVarP(&options.MessageSize, "size", "s", 1024, "the size of each message")
-	cmd.Flags().UintVarP(&options.Threads, "threads", "t", 1, "the numbers of worker threads")
-	cmd.Flags().Uint64VarP(&options.RequestPerSecond, "rps", "r", 1<<32, "the request per second for each worker")
+	cmd.Flags().UintVarP(&options.Threads, "threads", "t", 100, "the numbers of worker threads")
+	cmd.Flags().Uint64VarP(&options.RequestPerSecond, "rps", "r", 128, "the request per second for each worker")
 	return cmd
 }
 
